7a269514c6941431fab5a6a78ebe516330aa6583
CoderJ34 <bballclips238@gmail.com>
2024-11-24 18:05:43 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,34 @@
 set -e
 
 # Step 1: Install Python dependencies from requirements.txt
+apt-get update
+apt-get install -y \
+  wget \
+  gnupg \
+  curl \
+  libnss3 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libxss1 \
+  libgdk-pixbuf2.0-0 \
+  libnspr4 \
+  libxcomposite1 \
+  libxrandr2 \
+  libgbm1 \
+  libasound2 \
+  libpangocairo-1.0-0 \
+  libpango-1.0-0 \
+  libx11-xcb1 \
+  libgl1-mesa-glx \
+  libegl1-mesa \
+  libxrandr2 \
+  libgbm1 \
+  libgdk-pixbuf2.0-0 \
+  libsecret-1-0 \
+  libvulkan1 \
+  libavif-dev \
+  libgles2-mesa
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
